ceae359f49a813002b1b4e13ac2f96e9e99d725b
kobros-tech <alkobroslymohamed@gmail.com>
2023-07-06 16:27:00 +0300

    Finalizing Bostana Vendor Form

---
+++ bostana/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Bostana Vendor Form',
+    'version': '16.0',
+    'category': 'Purchase',
+    'website': 'https://royalconsultants.net',
+    'license': 'OPL-1',
+    'author': 'RoyalConsultants',
+    'summary': 'create a new selection field with a specific value of which we create and show up two other fields',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        "res_partner_views.xml",
+    ],
+}
