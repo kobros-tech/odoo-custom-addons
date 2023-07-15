a34302120d62a3bcf521a0261d227680749a5f63
kobros-tech <alkobroslymohamed@gmail.com>
2023-07-15 19:51:34 +0300

    internal reference customization

---
+++ custom_inventory/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Product Template Internal Refrence',
+    'version': '16.0',
+    'category': 'Sales/Sales',
+    'website': 'https://royalconsultants.net',
+    'license': 'OPL-1',
+    'author': 'RoyalConsultants',
+    'summary': 'create a new selection field with a specific value of which we create and show up two other fields',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        "product_template_views.xml",
+    ],
+}
---
+++ custom_inventory/product_template.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = "product.template"
+
+    default_code = fields.Char(
+        'Internal Very New Reference', compute='_compute_default_new_code')
+
+    # ----------------------------------- Constrains and Onchanges --------------------------------
+
+    @api.onchange("default_code")
+    def _compute_default_new_code(self):
+
+        # Make a list of all internal references
+        product_ids = self.env["product.template"].search([])
+        items = product_ids.mapped("default_code")
+
+        # Make sure if there are any internal references
+        # Then make sure if there are numeric internal references
+        num_items = []
+        if items:
+            if not self.default_code:
+                for item in items:
+                    is_num_item = str(item).isnumeric()
+                    if is_num_item:
+                        # Increment the list of only numeric internal reference by a new numeric item
+                        num_items.append(item)
+                # If there are no numeric internal references add a default one which is a million
+                # Else increment the maximum present number by 1
+                if not num_items:
+                    self.default_code = str("1000000")
+                else:
+                    new_item = int(max(num_items)) + 1
+                    self.default_code = str(new_item)
+
