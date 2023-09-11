b1485ffa86ed65b9a0364c52312f020e12ca93c2
kobros-tech <alkobroslymohamed@gmail.com>
2023-09-11 17:06:04 +0300

    adjusting footer and header for two reports

---
+++ printout_modify/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Printout Modify',
+
+    'version': '16.0',
+
+    'category': 'Printing',
+
+    'website': "https://royalconsultants.net",
+    'license': 'OPL-1',
+
+    'author': "RoyalConsultants",
+
+    'summary': '''
+        Printout Modify 
+        
+    ''',
+
+    'depends': ['base', 'sale'],
+
+    'data': [
+        'views/arab_pat_report.xml',
+        'views/aqua_pura_report.xml',
+    ]
+
+}
